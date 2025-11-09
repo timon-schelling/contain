1f9da14b8ece52d03606fa7c1941bb8a285323b0
Timon Schelling <me@timon.zip>
2025-11-09 00:33:55 +0000

    dep: update flake

--- nix/pkgs/crosvm-gpu-only/default.nix
+++ nix/pkgs/crosvm-gpu-only/default.nix
@@ -3,16 +3,6 @@ pkgs: pkgs.crosvm.overrideAttrs (oldAttrs: {
     ./disable-fbdev-support.patch
   ] ++ oldAttrs.patches;
 
-  postPatch = oldAttrs.postPatch or "" + ''
-    # see https://github.com/magma-gpu/rutabaga_gfx/issues/18
-    substituteInPlace $cargoDepsCopy/rutabaga_gfx-*/src/cross_domain/mod.rs \
-        --replace-fail \
-        "Ok(DescriptorType::WritePipe) => {
-                                        *identifier_type = CROSS_DOMAIN_ID_TYPE_VIRTGPU_BLOB;" \
-        "Ok(DescriptorType::WritePipe) => {
-                                        *identifier_type = CROSS_DOMAIN_ID_TYPE_WRITE_PIPE;"
-  '';
-
   buildNoDefaultFeatures = true;
   buildFeatures = [
     "gpu"
